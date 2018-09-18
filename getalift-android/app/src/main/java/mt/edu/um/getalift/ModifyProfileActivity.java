71bb0ee19006700444452ffd9b487b4b3dfeb103
Thessalene <thessalene.jeanlouis@reseau.eseo.fr>
2018-09-18 16:27:50 +0200

    Page settings en cours
    Changement de langue apres appui sur valider ok
    Acceder a une nouvelle activite (qui servira a modifier les infos du user) ok
    Plusieurs categories ok

---
+++ getalift-android/app/src/main/java/mt/edu/um/getalift/ModifyProfileActivity.java
@@ -0,0 +1,19 @@
+package mt.edu.um.getalift;
+
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class ModifyProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_profile);
+
+    }
+
+
+}
--- getalift-android/app/src/main/java/mt/edu/um/getalift/SettingsActivity.java
+++ getalift-android/app/src/main/java/mt/edu/um/getalift/SettingsActivity.java
@@ -5,14 +5,18 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.preference.ListPreference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.Locale;
 
@@ -39,60 +43,21 @@ public class SettingsActivity extends AppCompatActivity {
 
 
         mValidButtonSettings = (Button) findViewById(R.id.btn_valid_settings);
-        mChangeLanguageButton = (Button) findViewById(R.id.btn_change_language);
 
         // Display the settings screen
         getFragmentManager().beginTransaction().replace(R.id.content_frame, new MyPreferenceFragment()).commit();
 
-        //Display alert dialog to choose the language
-        mChangeLanguageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showChangeLanguageDialog();
-            }
-        });
 
         mValidButtonSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                choixLangue();
             }
         });
 
     }
 
-    private void showChangeLanguageDialog() {
-        final String[] listItem  ={"English","French","Spanish"};
-        AlertDialog.Builder mBuilder = new AlertDialog.Builder(SettingsActivity.this);
-        mBuilder.setTitle("Choose Language");
-        mBuilder.setSingleChoiceItems(listItem, -1, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                if (i == 0){
-                    //French
-                    setLocale("en");
-                    recreate();
-                }
-                else if (i == 1){
-                    //English
-                    setLocale("fr");
-                    recreate();
-                }
-                else if (i == 2){
-                    //Espagnol
-                    setLocale("es-rEs");
-                    recreate();
-                }
-                dialogInterface.dismiss();
-
-            }
-        });
-        AlertDialog mDialog = mBuilder.create();
-        //Show alert
-        mDialog.show();
-
-    }
-
+    //Change language
     private void setLocale(String lang) {
         Locale locale = new Locale(lang);
         Locale.setDefault(locale);
@@ -133,6 +98,31 @@ public class SettingsActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+
+    }
+
+
+    public void choixLangue() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String language = prefs.getString("change language", "Défaut");
+        Log.i("salut salut",language);
+
+        if(language.equals("fr")){
+            //French
+            Toast.makeText(getApplicationContext(), "Vous avez choisi :" + language, Toast.LENGTH_SHORT).show();
+            setLocale("fr");
+            recreate();
+        }else if(language.equals("en")){
+            //French
+            Toast.makeText(getApplicationContext(), "Vous avez choisi :" + language, Toast.LENGTH_SHORT).show();
+            setLocale("en");
+            recreate();
+        }if(language.equals("es")){
+            //French
+            Toast.makeText(getApplicationContext(), "Vous avez choisi :" + language, Toast.LENGTH_SHORT).show();
+            setLocale("es");
+            recreate();
+        }
 
     }
 
