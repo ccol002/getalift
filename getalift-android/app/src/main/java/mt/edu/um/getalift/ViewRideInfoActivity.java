206734c375e89be1389881aca3b4008e1bcfdae4
Thessalene <42738178+Thessalene@users.noreply.github.com>
2018-10-16 16:35:08 +0200

    Add passenger ok
    Ride exists ?
    - Yes : it add it and add the passenger
    - No : it not adds it and add the passenger if he is not yet passenger to this route

--- getalift-android/app/src/main/java/mt/edu/um/getalift/ViewRideInfoActivity.java
+++ getalift-android/app/src/main/java/mt/edu/um/getalift/ViewRideInfoActivity.java
@@ -59,6 +59,7 @@ public class ViewRideInfoActivity extends AppCompatActivity {
     private MyPoint endingPoint;
     private int routeId;
     private int userID;
+    private int ride_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -106,20 +107,22 @@ public class ViewRideInfoActivity extends AppCompatActivity {
         //Complete the fields with the info of the driver
         completeDriverInfo();
 
+        //Add ride and the passenger to the ride by clicking on this button
         btn_confirm_ride.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                createPassenger();
-                //RecoverRideId();
+                addRide();
+
             }
         });
 
     }
 
-
-    private void RecoverRideId(){
+//When the user click on "Confirm", the app creates a ride corresponding to the route selected before
+    //To add the route to the ride table in the database :
+    private void addRide(){
         final int route_id = routeId ;
-        final String[] ride = new String[1];
+
         // We first setup the queue for the API Request
         RequestQueue queue = Volley.newRequestQueue(this);
         // We get the URL of the server.
@@ -131,12 +134,33 @@ public class ViewRideInfoActivity extends AppCompatActivity {
                 {
                     @Override
                     public void onResponse(String response) {
-                        // response
-                        Log.d("Response", response);
-                        ride[0] =response;
-                        Toast.makeText(getBaseContext(),"Ride ID Trouvée",Toast.LENGTH_SHORT).show();
-                        NavUtils.navigateUpFromSameTask(activity);
+                        try {
+                            Log.d("GetALift_1", response);
+                            // We create a JSONObject from the server response.
+                            JSONObject jo = new JSONObject(response);
+                            // If the server respond with a success...
+                            if (jo.getBoolean("success")){
+                                // We tell the user his ride is now created...
+                                Toast.makeText(getApplicationContext(), "Route added to your rides", Toast.LENGTH_SHORT).show();
+                                //Get the ride_Id created
+                                ride_id = jo.getInt("insertId");
+
+                                //Add the user as a passenger in this ride
+                                createPassenger(ride_id);
+                            } else {
+                                if (jo.getInt("errorCode") == 1){
+                                    Log.i("TAG_ErrorCode_1", "on rentre dans error code");
+                                    // We tell the user his account is now created...
+                                    Toast.makeText(getApplicationContext(), jo.getString("message"), Toast.LENGTH_SHORT).show();
+                                    addUserToExistingRide(route_id);
+                               }
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
+
                 },
                 new Response.ErrorListener()
                 {
@@ -150,8 +174,8 @@ public class ViewRideInfoActivity extends AppCompatActivity {
                 }
         ) {
 
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
+           @Override
+            public Map<String, String> getHeaders()/* throws AuthFailureError*/ {
                 HashMap<String, String> headers = new HashMap<String, String>();
                 //headers.put("Content-Type", "application/json");
                 //or try with this:
@@ -162,7 +186,7 @@ public class ViewRideInfoActivity extends AppCompatActivity {
 
             @Override
             //Parameters for the SQL request, we need all the information
-            protected Map<String, String> getParams() throws AuthFailureError {
+            protected Map<String, String> getParams()/* throws AuthFailureError*/ {
                 Map<String, String> params = new HashMap<String, String>();
                 //ride, passenger, inTheCar
                 params.put("route",Integer.toString(route_id));
@@ -173,12 +197,97 @@ public class ViewRideInfoActivity extends AppCompatActivity {
         };
 
         queue.add(putRequest);
-        //return ride[0];
+
 
     }
-    private void createPassenger() {
 
-        final int route_id = routeId;
+
+    //When the user click on confirm, the route is added in the ride table
+    //If the ride already exist, the addRide() send an error with the errorCode==1 and say  "This route is already attributed to a ride"
+    //
+    private void addUserToExistingRide(int route) {
+        final int route_id = route;
+        final int user_id = userID ;
+        // We first setup the queue for the API Request
+        RequestQueue queue = Volley.newRequestQueue(this);
+        // We get the URL of the server.
+        String url = ConnectionManager.SERVER_URL+"/api/passenger/existingRide";
+        final Activity activity = this;
+
+        StringRequest putRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            Log.d("GetALift_2_add_existing", response);
+                            // We create a JSONObject from the server response.
+                            JSONObject jo = new JSONObject(response);
+                            // If the server respond with a success...
+                            if (jo.has("insertId")) {
+                                Log.i("GetALift_2_error", jo.getString("serverStatus"));
+                                Toast.makeText(getApplicationContext(),"You have been added as a passenger on the route selected, you can see it in your lifts",Toast.LENGTH_LONG).show();
+                                NavUtils.navigateUpTo(activity, getParentActivityIntent());
+                            } else if(!jo.getBoolean("success")) {
+                                Log.i("GetALift_2_already", "Already passenger");
+                                // We tell the user his ride is now created...
+                                Toast.makeText(getBaseContext(),"You are already passenger on this ride !", Toast.LENGTH_SHORT).show();                                //Come Back
+                                NavUtils.navigateUpTo(activity, getParentActivityIntent());
+
+                            }
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("Error.Response", error.toString());
+                        Toast.makeText(getBaseContext(),"Error to add the user in an existing ride !", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+        ) {
+
+            @Override
+            public Map<String, String> getHeaders() /*throws AuthFailureError*/ {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                //headers.put("Content-Type", "application/json");
+                //or try with this:
+                headers.put("x-access-token", "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJkb2RvIiwicGFzc3dvcmQiOiIkMmIkMTAkTGhNLnVCZ1YyL2JkYW9nbHpRUkNVZS5XL2Z0QTdnUG5mdEp2NC5JWFlGeGtCamplNVhVOHEiLCJuYW1lIjoiZG9kbyIsInN1cm5hbWUiOiJkb2RvIiwiZW1haWwiOiJkb2RvQGdtYWlsLmNvbSIsIm1vYmlsZU51bWJlciI6IjA2MDYwNjA2MDYiLCJpc1ZlcmlmaWVkIjowfQ.kWqjMDwA6iwcNDXEYYzgHHnMwnCOwBHBX9aDHHi3gKo");
+                headers.put("Content-Type", "application/x-www-form-urlencoded");
+                return headers;
+            }
+
+            @Override
+            //Parameters for the SQL request, we need all the information
+            protected Map<String, String> getParams() /*throws AuthFailureError */{
+                Map<String, String> params = new HashMap<String, String>();
+                //ride, passenger, inTheCar
+                params.put("routeId",Integer.toString(route_id));
+                params.put("passId",Integer.toString(user_id));
+
+                Log.i("TAG_Route_id", Integer.toString(route_id));
+                Log.i("TAG_User_id", Integer.toString(user_id));
+
+                return params;
+
+            }
+        };
+
+        queue.add(putRequest);
+    }
+
+    //To add the passenger to the database
+    private void createPassenger(final int ride_id) {
+
+        //final int route_id = routeId;
         final int user_id = userID ;
         // We first setup the queue for the API Request
         RequestQueue queue = Volley.newRequestQueue(this);
@@ -191,8 +300,7 @@ public class ViewRideInfoActivity extends AppCompatActivity {
                 {
                     @Override
                     public void onResponse(String response) {
-                        // response
-                        Log.d("Response", response.toString());
+                        Log.d("GetALift_3", response.toString());
                         Toast.makeText(getBaseContext(),"Vous avez été ajouté en tant que passager, veuillez attendre la confirmation du conducteur",Toast.LENGTH_LONG).show();
                         NavUtils.navigateUpTo(activity, getParentActivityIntent());
                     }
@@ -224,7 +332,7 @@ public class ViewRideInfoActivity extends AppCompatActivity {
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String, String> params = new HashMap<String, String>();
                     //ride, passenger, inTheCar
-                params.put("ride","29");
+                params.put("ride",Integer.toString(ride_id));
                 params.put("passenger",Integer.toString(user_id));
                 params.put("inTheCar","0");
 
@@ -237,7 +345,7 @@ public class ViewRideInfoActivity extends AppCompatActivity {
 
     }
 
-    // Return to the last page
+    // Return to the last page when clicking on the arrow
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // If we select the "Go back" button
@@ -295,10 +403,9 @@ public class ViewRideInfoActivity extends AppCompatActivity {
             }
         };
         queue.add(sr);
-
     }
 
-
+//Convert the coordinates into addresses ti display them to the driver
     public static String getAddressFromLocation(final double latitude, final double longitude, final Context context) {
         Geocoder geocoder = new Geocoder(context, Locale.getDefault());
         String result = null;
