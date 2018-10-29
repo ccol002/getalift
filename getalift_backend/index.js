a16675558e1eb7a19d8892f61d4e003b0e0a2118
Thessalene <42738178+Thessalene@users.noreply.github.com>
2018-10-29 14:27:04 +0100

    index.js
    Modification : ne pas prendre en compte la date du jour

--- getalift_backend/index.js
+++ getalift_backend/index.js
@@ -966,7 +966,7 @@ router.get("/passenger/alert/:driverId", function(req, res){
 
 //Requête qui renvoie toutes les informations sur un passager par rapport à un driver
 router.get("/passenger/alert/withoutDateVerification/:driverId", function(req, res){
-	db_con.query("SELECT DISTINCT passenger.*, passager.username From User passager,User conducteur,Route route,Ride ride,Passenger passenger, RouteDate routeDate Where ride.route = route.id and passenger.ride = ride.id and conducteur.id = route.driver and passenger.passenger = passager.id and conducteur.id = ? and passenger.inTheCar = 0 and routeDate.route = route.id and routeDate.route_date < ?", [req.params.driverId, todayDate], function(err, result){
+	db_con.query("SELECT DISTINCT passenger.*, passager.username From User passager,User conducteur,Route route,Ride ride,Passenger passenger, RouteDate routeDate Where ride.route = route.id and passenger.ride = ride.id and conducteur.id = route.driver and passenger.passenger = passager.id and conducteur.id = ? and passenger.inTheCar = 0 and routeDate.route = route.id", [req.params.driverId], function(err, result){
 		if(err) throw err;
 		res.json(result);
 	});
