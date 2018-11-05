6bef548209b1012315699e0ae21fe0ed7493aa0b
alexandreDony <alexandre.dony@reseau.eseo.fr>
2018-11-05 15:10:28 +0100

    Rating

--- getalift-android/app/src/main/java/mt/edu/um/getalift/RatingSystem.java
+++ getalift-android/app/src/main/java/mt/edu/um/getalift/RatingSystem.java
@@ -51,7 +51,7 @@ public class RatingSystem extends AppCompatActivity {
 
     // Données à envoyer
     Date currentTime;
-    int rideId;
+    int role;
     int userID;
 
 
@@ -82,6 +82,8 @@ public class RatingSystem extends AppCompatActivity {
             Log.i(TAG + "target ID",String.valueOf(targetID));
             routeId = intentRating.getIntExtra("routeId",0);
             Log.i(TAG + "target ID",String.valueOf(routeId));
+            role = intentRating.getIntExtra("role",4);
+            Log.i(TAG + "role ID",String.valueOf(role));
         }
 
         // récupération de l'utilisateur actuel
@@ -114,7 +116,11 @@ public class RatingSystem extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                rateThis(view);
+                if (role == 0){
+                    rateDriver(view);
+                }else {
+                    ratePassenger(view);
+                }
                 Intent intent = new Intent(getApplicationContext(),ProfileActivity.class);
                 intent.putExtra("userId",targetID);
                 intent.putExtra("routeId",routeId);
@@ -134,7 +140,7 @@ public class RatingSystem extends AppCompatActivity {
 
     }
 
-    public void rateThis(View view){
+    public void ratePassenger(View view){
 
         // We first setup the queue for the API Request
         RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
@@ -175,13 +181,12 @@ public class RatingSystem extends AppCompatActivity {
                                 Log.d("GetALift", response);
                                 // We create a JSONObject from the server response.
                                 JSONObject jo = new JSONObject(response);
-                                Log.i(TAG,response);
                                 // If the server respond with a success...
                                 if (jo.getBoolean("success")) {
                                     // We tell the user his rate is created and back to the last page
                                     Toast.makeText(getApplicationContext(), getString(R.string.success_rate_created), Toast.LENGTH_SHORT).show();
                                     Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
-                                    intent.putExtra("userId", userID);
+                                    intent.putExtra("userId", targetID);
                                     intent.putExtra("routeId", routeId);
                                     intent.putExtra("canRate", 1);
                                     startActivity(intent);
@@ -190,7 +195,106 @@ public class RatingSystem extends AppCompatActivity {
                                         // We tell the user he can't rate 2 times
                                         Toast.makeText(getApplicationContext(), getString(R.string.error_rate_exists), Toast.LENGTH_SHORT).show();
                                         Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
-                                        intent.putExtra("userId", userID);
+                                        intent.putExtra("userId", targetID);
+                                        intent.putExtra("routeId", routeId);
+                                        intent.putExtra("canRate", 1);
+                                        startActivity(intent);
+                                    }
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.d("GetALift", error.toString());
+                        }
+                    }
+            ) {
+
+                @Override
+                public Map<String, String> getHeaders()
+                {
+                    Map<String, String> headers = new HashMap<String, String>();
+                    headers.put("x-access-token", "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJkb2RvIiwicGFzc3dvcmQiOiIkMmIkMTAkTGhNLnVCZ1YyL2JkYW9nbHpRUkNVZS5XL2Z0QTdnUG5mdEp2NC5JWFlGeGtCamplNVhVOHEiLCJuYW1lIjoiZG9kbyIsInN1cm5hbWUiOiJkb2RvIiwiZW1haWwiOiJkb2RvQGdtYWlsLmNvbSIsIm1vYmlsZU51bWJlciI6IjA2MDYwNjA2MDYiLCJpc1ZlcmlmaWVkIjowfQ.kWqjMDwA6iwcNDXEYYzgHHnMwnCOwBHBX9aDHHi3gKo");
+                    headers.put("Content-Type", "application/x-www-form-urlencoded");
+                    return headers;
+                }
+
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("author", author);
+                    params.put("target", target);
+                    params.put("routeId", ride);
+                    params.put("stars", stars);
+                    params.put("comment", comment);
+                    params.put("postDate", postDate);
+                    return params;
+                }
+            };
+            queue.add(sr);
+        }
+    }
+
+    public void rateDriver(View view){
+
+        // We first setup the queue for the API Request
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        // We get the URL of the server.
+        String url = ConnectionManager.SERVER_URL+"/api/ratings/existingRate/Driver";
+
+        // We retrieve what the user select in the form
+        final String comment = ((EditText) findViewById(R.id.editCommentaire)).getText().toString();
+        final String stars = String.valueOf((int)ratingBar.getRating());
+        final String author = String.valueOf(userID);
+        final String ride = String.valueOf(routeId);
+        final String target = String.valueOf(targetID);
+        //final String postDate = currentTime.toString();
+        final String postDate = "2018-11-05";
+
+        /*
+        Log.i(TAG,comment);
+        Log.i(TAG,stars);
+        Log.i(TAG,author);
+        Log.i(TAG,ride);
+        Log.i(TAG,target);
+        Log.i(TAG,postDate);
+        */
+
+        //On vérifie que les données ne sont pas vide ou incompatible au format
+        if (comment.length() < 5) {
+            Toast.makeText(getApplicationContext(), getString(R.string.error_commentRate_long), Toast.LENGTH_SHORT).show();
+        } else {
+            // We create the Request. It's a StringRequest, and not directly a JSONObjectRequest because
+            // it looks like it's more stable.
+            StringRequest sr = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>() {
+
+                        @Override
+                        public void onResponse(String response) {
+                            // We got a response from our server.
+                            try {
+                                Log.d("GetALift", response);
+                                // We create a JSONObject from the server response.
+                                JSONObject jo = new JSONObject(response);
+                                // If the server respond with a success...
+                                if (jo.getBoolean("success")) {
+                                    // We tell the user his rate is created and back to the last page
+                                    Toast.makeText(getApplicationContext(), getString(R.string.success_rate_created), Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+                                    intent.putExtra("userId", targetID);
+                                    intent.putExtra("routeId", routeId);
+                                    intent.putExtra("canRate", 1);
+                                    startActivity(intent);
+                                } else {
+                                    if (jo.getInt("errorCode") == 1) {
+                                        // We tell the user he can't rate 2 times
+                                        Toast.makeText(getApplicationContext(), getString(R.string.error_rate_exists), Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+                                        intent.putExtra("userId", targetID);
                                         intent.putExtra("routeId", routeId);
                                         intent.putExtra("canRate", 1);
                                         startActivity(intent);
